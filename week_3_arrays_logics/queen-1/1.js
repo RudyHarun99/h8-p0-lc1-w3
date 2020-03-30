0e7a2ef7adc2ca3a8f03c9bfa2e9efa071b1bbb1
RudyHarunArrasyid <rdyhrn@gmail.com>
2020-03-30 19:34:16 +0700

    'livecode1'

---
+++ week_3_arrays_logics/queen-1/1.js
@@ -0,0 +1,35 @@
+/**
+ * Seorang anak bernama Jansen sedang membaca artikel tentang Generasi Manusia. Setelah membaca artikel, 
+ * Jansen mengetahui bahwa setiap generasi ditentukan dari tahun kelahiran dan tahun itu memiliki range yang 
+ * berbeda-beda, seperti berikut:
+ *   -Generasi Baby Boomer, orang yang lahir pada tahun 1946 hingga 1964
+ *   -Generasi X, orang yang lahir pada tahun 1965 hingga 1980
+ *   -Generasi Y (Milenial), orang yang lahir pada tahun 1981 hingga 1994
+ *   -Generasi Z, orang yang lahir pada tahun 1995 hingga 2010
+ *   -Generasi Alpha, orang yang lahir pada tahun 2011 hingga 2024
+ * 
+ * Saat ini Jansen berumur 12 tahun. kesimpulan yang Jansen dapat setelah membaca
+ * artikel tersebut ialah "Jansen merupakan Generasi Z"
+ * 
+ * Buatlah Pseudocode untuk program diatas 
+ */
+
+/**
+ * Pseudocode :
+ * SET 'tahun' as integer
+ * SET 'nama' as string
+ * READ 'tahun'
+ * READ 'nama'
+ * IF 'tahun' >= 1946 AND 'tahun' <= 1964
+ *  PRINT '{nama} merupakan Generasi Baby Boomer'
+ *  ELSE IF 'tahun' >= 1965 AND 'tahun' <= 1980
+ *      PRINT '{nama} merupakan Generasi X'
+ *      ELSE IF 'tahun' >= 1981 AND 'tahun' <= 1994
+ *          PRINT '{nama} merupakan Generasi Y (Milenial)'
+ *          ELSE IF 'tahun' >= 1995 AND 'tahun' <= 2010
+ *              PRINT '{nama} merupakan generasi Z'
+ *              ELSE IF 'tahun' >= 2011 AND 'tahun' <=2024
+ *                  PRINT '{nama} merupakan Generasi ALpha'
+ * ENDIF
+ * 
+*/
---
+++ week_3_arrays_logics/queen-1/2.js
@@ -0,0 +1,63 @@
+/*
+Sebuah Toko perlengkapan bernama “FoxShop” sedang membagikan beberapa barang khusus bagi membernya.
+Pembeli dari barang - barang khusus ini diwajibkan membawa kartu keanggotaan FoxShop.
+Kartu keanggotaan terdiri dari 2 label berbeda yaitu Reguler dan VIP, dari setiap kartu memiliki 3 level yang berbeda.
+
+Barang yang akan dibagikan memiliki syarat berikut :
+1. kartu dengan level 1 akan mendapatkan kotak berwarna merah
+2. kartu dengan level 2 akan mendapatkan kotak berwarna biru
+3. kartu dengan level 3 akan mendapatkan kotak berwarna kuning
+4. kartu dengan label VIP akan mendapatkan kotak berukuran besar
+5. kartu dengan label Reguler akan mendapatkan kotak berukuran kecil
+6. jika tidak menginput kartu/nama/level, maka tampilkan 'Data tidak lengkap'
+7. jika level melebihi 3 atau jenis kartu berbeda dari yang di atas, maka tampilkan 'Invalid Card'
+
+Program ini akan menerima 3 input:
+Name -> di dalam variabel name
+Kartu -> di dalam variabel kartu
+Level  -> di dalam variabel level
+ 
+Jika kartu VIP dan level 2 , maka output:
+[Name] mendapatkan kotak berwarna biru berukuran besar.
+
+Jika input invalid meka output seperti yang di jelaskan pada syarat di atas
+
+Rules : 
+- Diperbolehkan membuat variable baru
+- Dilarang menggunakan built-in function
+- Dilarang menghapus dan mengubah nama dari variable yang tersedia
+*/
+
+
+var name = 'Rudy' // change variable name value to any String
+var kartu = 'premium' // change variable kartu to 'VIP' or 'Reguler' or ''
+var level = 1 // change variable level value to any Number 
+
+// Write your code below
+var vip = 'besar', reg = 'kecil', lev1 = 'merah', lev2 = 'biru', lev3 = 'kuning';
+
+if (name == '' || kartu == '' || level < 1) {
+    console.log('Data tidak lengkap');
+} else if (kartu == 'VIP') {
+        if (level == 1) {
+            console.log(`${name} mendapatkan kotak berwarna ${lev1} berukuran ${vip}`);
+        }   else if (level == 2) {
+            console.log(`${name} mendapatkan kotak berwarna ${lev2} berukuran ${vip}`)
+        }   else if (level == 3) {
+            console.log(`${name} mendapatkan kotak berwarna ${lev3} berukuran ${vip}`)
+        }   else {
+            console.log(`Invalid Card`);
+        }
+    }   else if (kartu == 'Reguler') {
+        if (level == 1) {
+            console.log(`${name} mendapatkan kotak berwarna ${lev1} berukuran ${reg}`);
+        }   else if (level == 2) {
+            console.log(`${name} mendapatkan kotak berwarna ${lev2} berukuran ${reg}`)
+        }   else if (level == 3) {
+            console.log(`${name} mendapatkan kotak berwarna ${lev3} berukuran ${reg}`)
+        }   else {
+            console.log(`Invalid Card`);
+        }
+    } else {
+        console.log(`Invalid Card`)
+    }
